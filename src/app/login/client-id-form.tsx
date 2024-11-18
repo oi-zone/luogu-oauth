96783ef3973230b5583488a3eaae3fc442daa199
Xinhe Wang <wangxinhe06@gmail.com>
2024-11-18 23:17:46 +0000

    Client ID login form UI

--- src/app/login/client-id-form.tsx
+++ src/app/login/client-id-form.tsx
@@ -32,33 +32,37 @@ export default function ClientIdForm({
     <Form {...form}>
       {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
       <form onSubmit={form.handleSubmit(action)}>
-        <FormField
-          control={form.control}
-          name="uid"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>用户 ID</FormLabel>
-              <FormControl>
-                <Input placeholder="_uid" type="number" {...field} min={1} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="clientId"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Client ID</FormLabel>
-              <FormControl>
-                <Input placeholder="__client_id" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">登录</Button>
+        <div className="grid gap-4">
+          <FormField
+            control={form.control}
+            name="uid"
+            render={({ field }) => (
+              <FormItem className="grid gap-2">
+                <FormLabel>用户 ID</FormLabel>
+                <FormControl>
+                  <Input placeholder="_uid" type="number" {...field} min={1} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="clientId"
+            render={({ field }) => (
+              <FormItem className="grid gap-2">
+                <FormLabel>Client ID</FormLabel>
+                <FormControl>
+                  <Input placeholder="__client_id" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="w-full">
+            登录
+          </Button>
+        </div>
       </form>
     </Form>
   );
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,29 @@
 import type { SearchParams } from "next/dist/server/request/search-params";
 import { loginWithClientId } from "./actions";
 import ClientIdForm from "./client-id-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default async function Page(props: {
   searchParams: Promise<SearchParams>;
 }) {
   const searchParams = await props.searchParams;
-  return <ClientIdForm action={loginWithClientId.bind(null, searchParams)} />;
+  return (
+    <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">登录</CardTitle>
+        <CardDescription>
+          请输入您的 Client ID 以授权登录您的洛谷账号
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ClientIdForm action={loginWithClientId.bind(null, searchParams)} />
+      </CardContent>
+    </Card>
+  );
 }
