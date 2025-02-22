45dcd02211e81b74365d3be3edfb2d76f6157767
Xinhe Wang <wangxinhe06@gmail.com>
2025-02-22 03:32:26 +0000

    test(e2e): verification code refresh

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -5,6 +5,10 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("login with client id", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByRole("tab", { name: "Cookie 登录" }).click();
+  });
+
   test.describe("form validation", () => {
     test("uid", async ({ page }) => {
       const uidInput = page.getByLabel("用户 ID");
@@ -37,5 +41,38 @@ test.describe("login with client id", () => {
       await submit.click();
       await expect(clientIdInput).toHaveAccessibleErrorMessage("");
     });
+  });
+});
+
+test.describe("login with verification code", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByRole("tab", { name: "验证码登录" }).click();
+  });
+
+  test.describe("verification code", () => {
+    test("refresh", async ({ page }) => {
+      const refresh = page.getByRole("button", { name: "刷新" });
+      const codeInput = page.getByLabel("验证码", { exact: true });
+
+      await expect(refresh).toBeEnabled();
+      const code = await codeInput.inputValue();
+
+      await refresh.click();
+      await expect(refresh).toBeDisabled();
+      await expect(refresh).toBeEnabled();
+      expect(await codeInput.inputValue()).not.toBe(code);
+    });
+  });
+
+  test("form validation", async ({ page }) => {
+    const uidInput = page.getByLabel("用户 ID");
+    const submit = page.getByRole("button", { name: "检查" });
+
+    await submit.click();
+    await expect(uidInput).toHaveAccessibleErrorMessage(/.+/);
+
+    await uidInput.fill("0");
+    await submit.click();
+    await expect(uidInput).toHaveAccessibleErrorMessage(/.+/);
   });
 });
