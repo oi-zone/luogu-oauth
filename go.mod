19436bfe137ba172f3dace64b290b24a550d3e3d
Xinhe Wang <wangxinhe06@gmail.com>
2025-02-23 04:26:32 +0000

    feat(api): bearer authorization

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/wxh06/luogu-oauth
 
 go 1.24
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/shopspring/decimal v1.4.0
+	github.com/steebchen/prisma-client-go v0.46.0
+)
+
+require go.mongodb.org/mongo-driver/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -1,14 +1,53 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/wxh06/luogu-oauth/prisma/db"
 )
 
+func getSession(ctx context.Context, client *db.PrismaClient, accessToken string) (*db.LuoguSessionModel, error) {
+	token, err := client.Token.
+		FindUnique(db.Token.AccessToken.Equals(accessToken)).
+		Select(db.Token.AccessTokenExpiresAt.Field(), db.Token.Revoked.Field()).
+		With(
+			db.Token.User.Fetch().Select().With(
+				db.LuoguUser.Sessions.
+					Fetch(db.LuoguSession.Valid.Equals(true)).
+					Select(db.LuoguSession.UID.Field(), db.LuoguSession.ClientID.Field()).
+					Take(1),
+			),
+		).
+		Exec(ctx)
+	if errors.Is(err, db.ErrNotFound) || token.Revoked || token.AccessTokenExpiresAt.Before(time.Now()) {
+		return nil, errors.New("Unauthorized")
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &token.User().Sessions()[0], nil
+}
+
 func main() {
-	rpURL, err := url.Parse("https://www.luogu.com")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	client := db.NewClient()
+	if err := client.Prisma.Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	rpURL, err := url.Parse("https://www.luogu.com.cn")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -18,6 +57,19 @@ func main() {
 			r.SetURL(rpURL)
 			r.Out.Header.Set("x-luogu-type", "content-only")
 			r.Out.Header.Set("x-lentille-request", "content-only")
+
+			r.Out.Header.Del("Authorization")
+			authorization := strings.Split(r.In.Header.Get("Authorization"), " ")
+			if authorization[0] == "Bearer" {
+				ctx := context.Background()
+				session, err := getSession(ctx, client, authorization[1])
+				if err != nil {
+					log.Println(err)
+					return
+				}
+				r.Out.AddCookie(&http.Cookie{Name: "_uid", Value: strconv.Itoa(session.UID)})
+				r.Out.AddCookie(&http.Cookie{Name: "__client_id", Value: session.ClientID})
+			}
 		},
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
